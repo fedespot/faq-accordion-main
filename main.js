e23dc6f208466ff82264213a902bcbf4d20e9e10
Federico Caro <37274650+fedespot@users.noreply.github.com>
2023-06-28 15:11:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,24 @@
 
 let questionTitle = document.getElementsByClassName('question__title');
-let arrow = document.querySelector('.arrow');
+
 
 for (i=0; i<questionTitle.length;i++){
     questionTitle[i].addEventListener('click', function(){
         this.classList.toggle('activa');
         let question__paragraph = this.nextElementSibling;
+        let arrow = this.firstChild.nextElementSibling
         
         
         if(question__paragraph.style.display === "block"){
             question__paragraph.style.display = "none";
-            
-            arrow.classList.remove('arrow_up');
-           // arrow.forEach((i) => i.classList.remove('arrow_up'));
-            
+            arrow.classList.remove('arrow_up');  
         }else{
             question__paragraph.style.display = "block"; 
-        
            arrow.classList.add('arrow_up');
-          // arrow.forEach((i) => i.classList.add('arrow_up'));
-        } 
-      
-            
+        }    
         
     })
 }
+
+
 
